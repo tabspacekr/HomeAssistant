265fc4494d3c6960cee4f3b3bc01ad3d7b67efd4
Terry Kong <82074684+tirasol@users.noreply.github.com>
2021-12-19 20:06:10 +0900

    Update HARRYPHOTO_HD_MO_01.php

--- shell_command/HARRYPHOTO_HD_MO_01.php
+++ shell_command/HARRYPHOTO_HD_MO_01.php
@@ -1,6 +1,7 @@
 <?php
-#sql서버 원격접속
+#mysql 접속 정보 기입
 $conn = mysqli_connect("api.tabspace.kr", "changeme", "changeme_password", "changeme");
+#데이터 insert를 위한 query
 $sql  = "INSERT INTO flow_rate_check_daily(sensor_name, sensor_description, daily_date, timecreated) VALUES('HARRYPHOTO_HD_MO_01', '해리포토 홍대점 모션센서 1번', date_format(CURRENT_TIMESTAMP, '%Y-%m-%d'), CURRENT_TIMESTAMP);";
 $result = mysqli_query($conn, $sql);
 if($result === false){
