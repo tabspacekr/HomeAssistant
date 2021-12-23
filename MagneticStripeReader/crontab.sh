4c4c82a3bae555ff33d4f56be3abc4df78c05a90
Terry Kong <82074684+tirasol@users.noreply.github.com>
2021-12-23 14:02:33 +0900

    Update crontab.sh

--- MagneticStripeReader/crontab.sh
+++ MagneticStripeReader/crontab.sh
@@ -1,6 +1,7 @@
 # 1분마다 실행 감시
-*/1 * * * * /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
-
+*/1 * * * * su - root - c /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
+# 부팅 시 자동실행
+@reboot su - root -c /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
 
 # 적용 후 Crontab 재시작 (crontab -e로 하였으면 안해주어도됨)
 # service crond restart
