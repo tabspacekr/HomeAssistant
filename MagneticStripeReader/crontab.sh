72b87eea6866192bd8423659072b7a79ffcfb93e
Terry Kong <82074684+tirasol@users.noreply.github.com>
2022-02-14 18:39:36 +0900

    Update crontab.sh
    로그 저장하지 않도록 수정

--- MagneticStripeReader/crontab.sh
+++ MagneticStripeReader/crontab.sh
@@ -1,5 +1,8 @@
 # 30초마다 실행 감시 (crontab은 1분이하로 설정이 불가능하여 30초 편법 구현)
-* * * * * /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
-* * * * * sleep 30; /root/msr_kill_protect.sh >> /root/msr_kill_protect.log     
+* * * * * /root/msr_kill_protect.sh
+* * * * * sleep 30; /root/msr_kill_protect.sh
+#* * * * * /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
+#* * * * * sleep 30; /root/msr_kill_protect.sh >> /root/msr_kill_protect.log   
 # 부팅 시 자동실행
-@reboot su - root -c /root/msr_kill_protect.sh >> /root/msr_kill_protect.log   
+@reboot su - root -c /root/msr_kill_protect.sh
+#@reboot su - root -c /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
