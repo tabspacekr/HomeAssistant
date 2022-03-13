c5d36756cea136e0a5f059a4816ccacc8bf4a47d
Terry Kong <82074684+tirasol@users.noreply.github.com>
2022-03-13 20:39:05 +0900

    Update crontab.sh

--- MagneticStripeReader/crontab.sh
+++ MagneticStripeReader/crontab.sh
@@ -1,8 +1,10 @@
 # 30초마다 실행 감시 (crontab은 1분이하로 설정이 불가능하여 30초 편법 구현)
 * * * * * /root/msr_kill_protect.sh
 * * * * * sleep 30; /root/msr_kill_protect.sh
+#로그 사용 시 주석 해제
 #* * * * * /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
 #* * * * * sleep 30; /root/msr_kill_protect.sh >> /root/msr_kill_protect.log   
 # 부팅 시 자동실행
 @reboot su - root -c /root/msr_kill_protect.sh
+#로그 사용 시 주석 해제
 #@reboot su - root -c /root/msr_kill_protect.sh >> /root/msr_kill_protect.log
