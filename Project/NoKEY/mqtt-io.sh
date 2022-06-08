348d3b5fff521fbf89991ec03c8c9f83824c7366
Terry Kong <82074684+tirasol@users.noreply.github.com>
2022-06-08 17:42:52 +0900

    Update mqtt-io.sh
    네트워크 체크 과정 추가

--- Project/NoKEY/mqtt-io.sh
+++ Project/NoKEY/mqtt-io.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# ip server
+serverAdr="nokey.tabspace.kr"
+
+ping -c 1 $serverAdr > /dev/null 2>&1
+while [ $? -ne 0 ]; do
+  echo -e "\e[1A\e[K $(date): Connecting - ${serverAdr}"
+  sleep 1
+  ping -c 1 $serverAdr > /dev/null 2>&1
+done
+
+echo "$(date): Connected - ${serverAdr}";
+rm /root/config.yml
 cp /root/config.yml_original /root/config.yml
 pkill -9 -f "python3 -m mqtt_io /root/config.yml"
 HOSTNAME=`echo NK$(ifconfig eth0|grep ether |awk '{print $2}'|cut -f 1-6 -d':'| tr [:lower:] [:upper:] | sed -e 's/://g')`
