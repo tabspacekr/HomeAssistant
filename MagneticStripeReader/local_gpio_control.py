aa39476900963da19a610a583c7805d72bda0582
Terry Kong <82074684+tirasol@users.noreply.github.com>
2022-07-01 16:25:11 +0900

    Update local_gpio_control.py

--- MagneticStripeReader/local_gpio_control.py
+++ MagneticStripeReader/local_gpio_control.py
@@ -3,6 +3,7 @@ from time import sleep          # this lets us have a time delay
 
 GPIO.setboard(GPIO.PCPCPLUS)    # Orange Pi PC board
 GPIO.setmode(GPIO.BOARD)        # set up BOARD BCM numbering
+GPIO.setwarnings(False)
 GPIO.setup(11, GPIO.OUT)         # set BCM11 as an output (RELAY)
 
 try:
@@ -22,5 +23,5 @@ try:
 
 except KeyboardInterrupt:
     GPIO.output(11, 0)           # set port/pin value to 0/LOW/False
-    GPIO.cleanup()              # Clean GPIO
+    #GPIO.cleanup()              # Clean GPIO
     print ("Bye.")
